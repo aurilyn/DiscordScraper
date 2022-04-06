e2ecf02156307ccb07ee6211c6396b9df07ac442
aurilyn <100726036+aurilyn@users.noreply.github.com>
2022-04-06 00:18:29 -0700

    Update discord.py

--- discord.py
+++ discord.py
@@ -4,7 +4,7 @@ import string
 
 def retrieve_message(channel_id):
     headers = {
-        'authorization' : 'MTk3NTU1NjM2NTYzNzM4NjI0.YiPT9Q.fwCdj-T8lp0BZB6WxwvvRwV8Nyo'
+        'authorization' : ''
     }
     r = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages?limit=150', headers=headers)
     jsonn = json.loads(r.text)
